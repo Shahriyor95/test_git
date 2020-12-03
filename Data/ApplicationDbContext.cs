83e1984b9902a5f76a14bf28d0185cbf34392cdc
Shaxriyor Komilov <sh.komilov@ung.uz>
2020-12-03 09:42:35 +0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TMS_project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+          
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=127.0.0.1;Port=5432;Database=TMS_project;Username=postgres;Password=123456789");
+        }
+    }
+}
